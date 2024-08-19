6247a932deb459368e7903b3988c657ba1f55b99
mohamed9nasser <medoxx490@gmail.com>
2024-08-20 01:21:58 +0300

    session2 task upload

---
+++ nav.php
@@ -0,0 +1,31 @@
+<?php
+
+echo ' <nav class="navbar navbar-expand-lg custom_nav-container ">
+            <a class="navbar-brand" href="index.php">
+              <span>
+                Inance
+              </span>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class=""> </span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about.php"> About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="service.php">Services</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="contact.php">Contact Us</a>
+                </li>
+              </ul>
+            </div>
+          </nav>';
+
